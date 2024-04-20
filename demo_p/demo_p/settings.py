f0bdfa2b0dd5df87e5bc20ab190d4907dbb05dd9
Kathirvel G <117812222+kathirvel-G@users.noreply.github.com>
2024-04-20 21:00:43 +0530

    Update settings.py

--- demo_p/demo_p/settings.py
+++ demo_p/demo_p/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='kathirvelganesan2468@gmail.com'
-EMAIL_HOST_PASSWORD='qtmk yrjq ftpk njxg'
+EMAIL_HOST_PASSWORD='#YOUR GOOGLE PASSWORD KEY#'
